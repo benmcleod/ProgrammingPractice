0c0073da29459e3508ef768d0a385ff9cff2f774
benmcleod <benfmcleod@gmail.com>
2018-09-19 13:24:14 -0400

    Add files via upload

---
+++ EuclideanDistance.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace EuclideanDistance
+{
+    class EuclideanDistance
+    {
+        static void Main(string[] args)
+        {
+            double lat = 48.8584;
+            double lon = 2.2945;
+
+            using (WebClient wc = new WebClient())
+            {
+                var client = new WebClient();
+                var response = client.DownloadString("https://opensky-network.org/api/states/all"); ;
+
+                RootObject m = JsonConvert.DeserializeObject<RootObject>(response);
+
+                var distance = 100000000.00;
+                var closestplane = new List<string>();
+
+                foreach(List<string> plane in m.states)
+                {
+                    double longitude, latitude;
+                    if (!(plane[5] == null || plane[6] == null))
+                    {
+                        longitude = double.Parse(plane[5]);
+                        latitude = double.Parse(plane[6]);
+
+                        var tempdistance = Distance(lat, lon, latitude, longitude);
+
+                        if (tempdistance < distance)
+                        {
+                            distance = tempdistance;
+                            closestplane = plane;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Distance : {0}", distance);
+                Console.WriteLine("Callsign : {0}", closestplane[1]);
+                Console.WriteLine("Lattitude : {0}, Longitude : {1}", closestplane[6], closestplane[5]);
+                Console.WriteLine("Altitude : {0}", closestplane[7]);
+                Console.WriteLine("Country of origin : {0}", closestplane[2]);
+                Console.WriteLine("ICAO24 ID : {0}", closestplane[0]);
+
+            }
+        }
+
+        private static double Distance(double lat, double lon, double latitude, double longitude)
+        {
+            return Math.Sqrt(Math.Pow((latitude - lat), 2) + Math.Pow((longitude - lon), 2));
+        }
+    }
+
+    public class RootObject
+    {
+        public int time { get; set; }
+        public List<List<string>> states { get; set; }
+    }
+}
---
+++ Tally.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TallyProgram
+{
+    class Tally
+    {
+        static void Main(string[] args)
+        {
+
+            TallyString("EbAAdbBEaBaaBBdAccbeebaec");
+
+            TallyStringLinq("EbAAdbBEaBaaBBdAccbeebaec");
+            
+
+        }
+
+
+
+        private static void TallyStringLinq(string v)
+        {
+            var scores = v
+                .Where(char.IsLetter)
+                .GroupBy(x => x)
+                .Select(x => new { character = x.First().ToString(), score = (char.IsLower(x.First())) ? x.Count() : -1 * x.Count() })
+                .GroupBy(x => x.character.ToLower())
+                .Select(x => new { player = x.First().character.ToLower(), score = x.Sum(xx => xx.score) })
+                .OrderByDescending(x => x.score);
+
+
+            Console.WriteLine(string.Join(", ", scores.Select(x => string.Format("{0}:{1}", x.player, x.score))));
+
+        }
+
+        private static void TallyString(string a)
+        {
+            KeyVal[] i = new KeyVal[5];
+            i[0] = new KeyVal('a', 0);
+            i[1] = new KeyVal('b', 0);
+            i[2] = new KeyVal('c', 0);
+            i[3] = new KeyVal('d', 0);
+            i[4] = new KeyVal('e', 0);
+
+            foreach (var c in a)
+            {
+                if ((int)c >= (int)'a')
+                {
+                    i[(int)c - (int)'a'].Count++;
+                }
+                else i[(int)c - (int)'A'].Count--;
+            }
+
+            Array.Sort(i);
+
+            Console.WriteLine(string.Join<KeyVal>(", ", i));
+        }
+    }
+    
+
+    public class KeyVal : IComparable<KeyVal>
+    {
+        public char Id { get; set; }
+        public int Count { get; set; }
+
+        public KeyVal() { }
+
+        public KeyVal(char id, int count)
+        {
+            this.Id = id;
+            this.Count = count;
+        }
+
+        public int CompareTo(KeyVal other)
+        {
+            if (this.Count == other.Count)
+            {
+                return this.Id.CompareTo(other.Id);
+            }
+            return other.Count.CompareTo(this.Count);
+        }
+
+        public override string ToString()
+        {
+            return string.Format(Id + ":" + Count);
+        }
+    }
+}
